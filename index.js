e3376d73216de13b06ce6e4fa4a277fcb0da1790
maing1 <faithmaingi1622@gmail.com>
2024-09-30 20:58:50 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+    const win = record.find(record => record.result === "W");
+    if (win) {
+        return win.year;
+    }
+    return undefined;
+}
+  
+  console.log(record.find(superbowlWin))
+   
